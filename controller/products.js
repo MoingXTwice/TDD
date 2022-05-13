12b95cdd4664fd396684f89dd09dca30350d9b20
MoingXTwice <sckbs1314@gmail.com>
2022-05-13 18:18:41 +0900

    Create 에러 처리

--- controller/products.js
+++ controller/products.js
@@ -1,6 +1,10 @@
 const Product = require('../models/Product');
 
-exports.createProduct = (req, res, next) => {
-    const createdProduct = Product.create(req.body);
-    res.status(201).json(createdProduct);
+exports.createProduct = async (req, res, next) => {
+    try {
+        const createdProduct = await Product.create(req.body);
+        res.status(201).json(createdProduct);
+    } catch (e) {
+        next(e);
+    }
 }
--- test/unit/products.test.js
+++ test/unit/products.test.js
@@ -11,8 +11,8 @@ let next;
 beforeEach(() => {
     req = httpMocks.createRequest();
     res = httpMocks.createResponse();
-    next = null;
-})
+    next = jest.fn();
+});
 
 describe('Product Controller Create', () => {
     beforeEach(() => {
@@ -21,18 +21,25 @@ describe('Product Controller Create', () => {
     it('should have a createProduct function', () => {
         expect(typeof productController.createProduct).toBe('function');
     });
-    it('should call Product.create', () => {
-        productController.createProduct(req, res, next);
+    it('should call Product.create', async () => {
+        await productController.createProduct(req, res, next);
         expect(Product.create).toBeCalledWith(newProduct);
     });
-    it('should return 201 response code', () => {
-        productController.createProduct(req, res, next);
+    it('should return 201 response code', async () => {
+        await productController.createProduct(req, res, next);
         expect(res.statusCode).toBe(201);
         expect(res._isEndCalled).toBeTruthy();
     });
-    it('should return json body in response', () => {
+    it('should return json body in response', async () => {
         Product.create.mockReturnValue(newProduct);
-        productController.createProduct(req, res, next);
+        await productController.createProduct(req, res, next);
         expect(res._getJSONData()).toStrictEqual(newProduct);
+    });
+    it('should handle errors', async () => {
+        const errorMessage = { message: 'description property missing' };
+        const rejectedPromise = Promise.reject(errorMessage);
+        Product.create.mockReturnValue(rejectedPromise);
+        await productController.createProduct(req, res, next);
+        expect(next).toBeCalledWith(errorMessage);
     });
 });
